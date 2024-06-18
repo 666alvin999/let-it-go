c86581b95b0cbfd396b03266ff45a5d0d8a10838
Alvin Hamaïde <alvin98mc@gmail.com>
2024-06-18 12:13:18 +0200

    project init

---
+++ src/main/java/org/letitgo/LetItGoApplication.java
@@ -0,0 +1,13 @@
+package org.letitgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetItGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetItGoApplication.class, args);
+	}
+
+}
